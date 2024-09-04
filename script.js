22216afadacd885bf0036ae0de08fcae4e3743b5
researchmariam <researchmariam@gmail.com>
2024-09-04 12:01:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuLinks = document.querySelectorAll('.menu-item a');
-    
+
     menuLinks.forEach(link => {
         link.addEventListener('click', function(event) {
-            event.preventDefault();
-            
             const sectionId = this.id.replace('-link', '-section');
             
+            if (sectionId !== window.location.hash.replace('#', '')) {
+                event.preventDefault();
+            }
+
             document.querySelectorAll('main section').forEach(section => {
                 section.classList.add('hidden');
             });
-            
+
             const section = document.getElementById(sectionId);
             section.classList.remove('hidden');
 
@@ -18,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
             window.scrollTo({ top: 0, behavior: 'smooth' });
         });
     });
-    
+
     // Check if there's a section ID in the URL hash
     const hash = window.location.hash;
     if (hash) {
